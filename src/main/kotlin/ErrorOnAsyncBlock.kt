95499d4cfe9fb24ae32296353f00fe401620a31c
nyandora <61850164+nyandora@users.noreply.github.com>
2021-05-05 21:40:06 +0900

    Initial commit

---
+++ src/main/kotlin/ErrorOnAsyncBlock.kt
@@ -0,0 +1,35 @@
+import kotlinx.coroutines.*
+import java.lang.Exception
+
+fun main() {
+    runBlocking {
+        try {
+            coroutineScope {
+                val deferred = async {
+                    delay(1000L)
+                    throw ArithmeticException()
+                    // this coroutine is marked "failed", so the parent coroutine is also marked "failed".
+                }
+                async {
+                    try {
+                        delay(2000L)
+                        println("this is never printed.")
+                    } catch (e: CancellationException) {
+                        println("cancelled caused by 1st async call.")
+                    }
+                }
+                println("waiting...")
+                try {
+                    deferred.await()
+                } catch (ae: java.lang.ArithmeticException) {
+                    println("the exception thrown by 1st async call is caught.")
+                    throw ae
+                    // even though the exception is not thrown, coroutineScope() throws ArithmeticException.
+                    // because by a failure in 1st async call the parent coroutine is already marked "failed".
+                }
+            }
+        } catch (ae: java.lang.ArithmeticException) {
+            println("coroutineScope() throws the exception thrown in 1st async call.")
+        }
+    }
+}
